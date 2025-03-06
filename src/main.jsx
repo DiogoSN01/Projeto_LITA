acc73a28f1249172de881725bbd4efd95ba80b77
Daniel Coimbra <153375587+DanieCoimbra@users.noreply.github.com>
2025-03-06 19:32:08 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Formulario from "./Pages/Formulario";
+import Home from "./Pages/Home";
+import Parcerias from "./Pages/Parcerias";
+import Projetos from "./Pages/Projetos";
+import Calendly from "./Pages/Calendly";
+import Sobre from "./Pages/Sobre";
+import "./index.css"; // Caso tenha estilos globais
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Projetos" element={<Projetos />} />
+        <Route path="/Parcerias" element={<Parcerias />} />
+        <Route path="/Formulario" element={<Formulario />} />
+        <Route path="/Calendly" element={<Calendly />} />
+        <Route path="/Sobre" element={<Sobre />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
